5c6cb6a85a0a2f307cb5506a1d9c98fca44748e8
mosern <andreas@mosvoll.net>
2016-03-11 09:07:17 +0100

    resource auth fix

---
+++ AdmAspNet/AdmAspNet/Helpers/ResourceAuthorizeAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AdmAspNet.Helpers
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = true)]
+    public class ResourceAuthorizeAttribute : Thinktecture.IdentityModel.Mvc.ResourceAuthorizeAttribute
+    {
+
+        public ResourceAuthorizeAttribute() : base()
+        {
+
+        }
+        public ResourceAuthorizeAttribute(string action, params string[] resources)
+            : base(action, resources)
+        {
+
+        }
+
+        protected override void HandleUnauthorizedRequest(System.Web.Mvc.AuthorizationContext filterContext)
+        {
+            if (filterContext.HttpContext.Request.IsAuthenticated)
+            {
+                filterContext.Result = new System.Web.Mvc.HttpStatusCodeResult((int)System.Net.HttpStatusCode.Forbidden);
+            }
+            else
+            {
+                base.HandleUnauthorizedRequest(filterContext);
+            }
+        }
+    }
+}
