273b126d28f9aae74d516462ca274cee97e1ae0b
henningfredriksen <rhymfaxe2@hotmail.com>
2016-03-17 16:39:58 +0100

    Addded ResourceManager

---
+++ VeiviserenUiTNarvik/app/src/main/java/no/hesa/veiviserenuitnarvik/ResourceManagerActivity.java
@@ -0,0 +1,13 @@
+package no.hesa.veiviserenuitnarvik;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ResourceManagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resource_manager);
+    }
+}
