8481cd2ea1e31c8233d8fb17e873eca78a2f8bbe
Henning Fredriksen <rhymf@DESKTOP-77VFA8F>
2016-04-09 14:18:35 +0200

    MainActivity, menu_main, activity_main, content_main cleanup

--- VeiviserenUiTNarvik/app/src/main/java/no/hesa/veiviserenuitnarvik/MainActivity.java
+++ VeiviserenUiTNarvik/app/src/main/java/no/hesa/veiviserenuitnarvik/MainActivity.java
@@ -1,72 +1,13 @@
 package no.hesa.veiviserenuitnarvik;
 
-import android.app.DownloadManager;
-
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.database.Cursor;
-import android.graphics.PointF;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
-import android.os.Looper;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.davemorrissey.labs.subscaleview.ImageSource;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.indooratlas.android.sdk.IALocation;
-import com.indooratlas.android.sdk.IALocationListener;
-import com.indooratlas.android.sdk.IALocationManager;
-import com.indooratlas.android.sdk.IALocationRequest;
-import com.indooratlas.android.sdk.IARegion;
-import com.indooratlas.android.sdk.resources.IAFloorPlan;
-import com.indooratlas.android.sdk.resources.IALatLng;
-import com.indooratlas.android.sdk.resources.IAResourceManager;
-import com.indooratlas.android.sdk.resources.IAResult;
-import com.indooratlas.android.sdk.resources.IAResultCallback;
-import com.indooratlas.android.sdk.resources.IATask;
 
-import java.io.File;
-
-public class MainActivity extends AppCompatActivity /*implements OnMapReadyCallback*/ {
+public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-    // blue dot radius in meters
-    private static final float dotRadius = 1.0f;
-
-    private GoogleMap mMap;
-    private Marker mMarker;
-
-    private SupportMapFragment mapFragment;
-
-    private IALocationManager mIALocationManager;
-    private IAResourceManager mFloorPlanManager;
-    private IATask<IAFloorPlan> mPendingAsyncResult;
-    private IAFloorPlan mFloorPlan;
-    private BlueDotView mImageView;
-    private long mDownloadId;
-    private DownloadManager mDownloadManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,35 +17,6 @@ public class MainActivity extends AppCompatActivity /*implements OnMapReadyCallb
         Intent myIntent = new Intent(MainActivity.this, MapActivity.class);
 //        myIntent.putExtra("key", value); //Optional parameters
         MainActivity.this.startActivity(myIntent);
-
-//        mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
-//        mImageView = (BlueDotView) findViewById(R.id.blue_dot_view);
-        /*
-        mDownloadManager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
-        mIALocationManager = IALocationManager.create(this);
-        mFloorPlanManager = IAResourceManager.create(this);
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        String floorplanid = getResources().getString(R.string.indooratlas_floor_1_floorplanid);
-        fetchFloorPlan(floorplanid);
-
-        // get map fragment reference
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-
-*/
     }
 
     @Override
@@ -116,47 +28,11 @@ public class MainActivity extends AppCompatActivity /*implements OnMapReadyCallb
     @Override
     protected void onResume() {
         super.onResume();
-        /*
-        if (mMap == null) {
-            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
-                    .getMap();
-        }
-        registerReceiver(onComplete, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
-        */
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        if (id == R.id.action_measurement)
-        {
-            startActivity(new Intent(getApplicationContext(),MeasurementActivity.class));
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-//        unregisterReceiver(onComplete);
     }
 
     @Override
@@ -168,131 +44,4 @@ public class MainActivity extends AppCompatActivity /*implements OnMapReadyCallb
     protected void onDestroy() {
         super.onDestroy();
     }
-/*
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        if (mImageView != null && mImageView.isReady())
-        {
-            com.indooratlas.android.sdk.resources.IALatLng latLng = new IALatLng(68.436135, 17.434950);
-            PointF point = mFloorPlan.coordinateToPoint(latLng);
-            mImageView.setDotCenter(point);
-            mImageView.postInvalidate();
-        }
-
-        // Add a marker in Narvik and move the camera
-        LatLng hin = new LatLng(68.436135, 17.434950);
-        mMap.addMarker(new MarkerOptions().position(hin).title("UiT Narvik"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(hin, 17));
-
-    }
-*/
-    /*  Broadcast receiver for floor plan image download */
-/*    private BroadcastReceiver onComplete = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            long id = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, 0L);
-            if (id != mDownloadId) {
-                Log.w(TAG, "Ignore unrelated download");
-                return;
-            }
-            Log.w(TAG, "Image download completed");
-            Bundle extras = intent.getExtras();
-            DownloadManager.Query q = new DownloadManager.Query();
-            q.setFilterById(extras.getLong(DownloadManager.EXTRA_DOWNLOAD_ID));
-            Cursor c = mDownloadManager.query(q);
-
-            if (c.moveToFirst()) {
-                int status = c.getInt(c.getColumnIndex(DownloadManager.COLUMN_STATUS));
-                if (status == DownloadManager.STATUS_SUCCESSFUL) {
-                    // process download
-                    String filePath = c.getString(c.getColumnIndex(
-                            DownloadManager.COLUMN_LOCAL_FILENAME));
-                    showFloorPlanImage(filePath);
-                }
-            }
-            c.close();
-        }
-    };
-
-    private void showFloorPlanImage(String filePath) {
-        Log.w(TAG, "showFloorPlanImage: " + filePath);
-
-//        float f = mFloorPlan.getMetersToPixels() * dotRadius;
-//        mapFragment.setRadius(mFloorPlan.getMetersToPixels() * dotRadius);
-//        mImageView.setRadius(mFloorPlan.getMetersToPixels() * dotRadius);
-//        mapFragment.setImage(ImageSource.uri(filePath));
-//        mImageView.setImage(ImageSource.uri(filePath));
-    }
-*/
-    /**
-     * Fetches floor plan data from IndoorAtlas server. Some room for cleaning up!!
-     */
-/*    private void fetchFloorPlan(String id) {
-        cancelPendingNetworkCalls();
-        final IATask<IAFloorPlan> asyncResult = mFloorPlanManager.fetchFloorPlanWithId(id);
-        mPendingAsyncResult = asyncResult;
-        if (mPendingAsyncResult != null)
-        {
-            mPendingAsyncResult.setCallback(new IAResultCallback<IAFloorPlan>()
-            {
-                @Override
-                public void onResult(IAResult<IAFloorPlan> result)
-                {
-                    Log.d(TAG, "fetch floor plan result:" + result);
-                    if (result.isSuccess() && result.getResult() != null)
-                    {
-                        mFloorPlan = result.getResult();
-                        String fileName = mFloorPlan.getId() + ".img";
-                        String filePath = Environment.getExternalStorageDirectory() + "/" + Environment.DIRECTORY_DOWNLOADS + "/" + fileName;
-                        File file = new File(filePath);
-                        if (!file.exists())
-                        {
-                            // Toast.makeText(MainActivity.this, "entered IF", Toast.LENGTH_LONG).show();
-
-                            DownloadManager.Request request = new DownloadManager.Request(Uri.parse(mFloorPlan.getUrl()));
-                            request.setDescription("IndoorAtlas floor plan");
-                            request.setTitle("Floor plan");
-                            // requires android 3.2 or later to compile
-                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)
-                            {
-                                request.allowScanningByMediaScanner();
-                                request.setNotificationVisibility(DownloadManager.
-                                        Request.VISIBILITY_HIDDEN);
-                            }
-                            request.setDestinationInExternalPublicDir(Environment.
-                                    DIRECTORY_DOWNLOADS, fileName);
-
-                            mDownloadId = mDownloadManager.enqueue(request);
-
-                        }
-                        else
-                        {
-                            showFloorPlanImage(filePath);
-                        }
-                    }
-                    else
-                    {
-                        // do something with error
-                        if (!asyncResult.isCancelled())
-                        {
-                            Toast.makeText(MainActivity.this,
-                                    (result.getError() != null
-                                            ? "error loading floor plan: " + result.getError()
-                                            : "access to floor plan denied"), Toast.LENGTH_LONG)
-                                    .show();
-                        }
-                    }
-                }
-            }, Looper.getMainLooper()); // deliver callbacks in main thread
-        }
-    }
-
-    private void cancelPendingNetworkCalls() {
-        if (mPendingAsyncResult != null && !mPendingAsyncResult.isCancelled()) {
-            mPendingAsyncResult.cancel();
-        }
-    }
-    */
 }
