670b2a5142081397f64687ac1c28c78e3e9a8904
Simon Ingebrigtsen <thasimon@live.com>
2016-04-09 16:24:37 +0200

    Added login again

--- VeiviserenUiTNarvik/app/src/main/java/no/hesa/veiviserenuitnarvik/api/ApiAsyncTask.java
+++ VeiviserenUiTNarvik/app/src/main/java/no/hesa/veiviserenuitnarvik/api/ApiAsyncTask.java
@@ -1,7 +1,88 @@
 package no.hesa.veiviserenuitnarvik.api;
 
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.util.Pair;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
+
 /**
  * Created by ThaSimon on 09.04.2016.
  */
-public class ApiAsyncTask {
+public class ApiAsyncTask extends AsyncTask<List<Pair<String,String>>,Void,JSONObject>{
+    private ActionInterface actionInterface;
+    private String actionString;
+    private String url;
+    private String dataType;
+    public ApiAsyncTask(ActionInterface actionInterface,String actionString, String url) {
+        this(actionInterface,actionString,url,"GET");
+    }
+
+    public ApiAsyncTask(ActionInterface actionInterface, String actionString,String url,String dataType) {
+        this.actionInterface = actionInterface;
+        this.actionString = actionString;
+        this.url = url;
+        this.dataType = dataType;
+    }
+    @Override
+    protected JSONObject doInBackground(List<Pair<String,String>>... params) {
+        JSONObject jObj = null;
+        try {
+            URL urlObject = new URL(url);
+            HttpsURLConnection conn = (HttpsURLConnection) urlObject.openConnection();
+            conn.setReadTimeout(10000);
+            conn.setConnectTimeout(15000);
+            conn.setRequestMethod(dataType);
+            conn.setDoInput(true);
+            conn.setDoOutput(false);
+            conn.setRequestProperty("Content-Type","application/x-www-form-urlencoded");
+            String query = getQueryString(params[0]);
+            OutputStream os = conn.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os,"UTF-8"));
+            writer.write(query);
+            writer.flush();
+            writer.close();
+            os.close();
+            int responseCode = conn.getResponseCode();
+            if (responseCode == HttpsURLConnection.HTTP_OK) {
+                String line;
+                StringBuilder sb = new StringBuilder();
+                BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(),"iso-8859-1"),8);
+                while ((line = br.readLine()) != null) {
+                    sb.append(line+"\n");
+                }
+                String json = sb.toString();
+                jObj = new JSONObject(json);
+            }
+
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return jObj;
+    }
+
+    @Override
+    protected void onPostExecute(JSONObject jsonObject) {
+        super.onPostExecute(jsonObject);
+        actionInterface.onCompletedAction(jsonObject,actionString);
+    }
+
+    private String getQueryString(List<Pair<String,String>> list) {
+        Uri.Builder builder = new Uri.Builder();
+        for (Pair<String,String> pair : list) {
+            builder.appendQueryParameter(pair.first,pair.second);
+        }
+        return builder.build().getEncodedQuery();
+    }
 }
