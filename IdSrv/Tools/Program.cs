f6aa96f741cada21feedecc382c7fdc0918ba76f
mosern <andreas@mosvoll.net>
2016-05-23 17:24:25 +0200

    provider identifier hashed

--- IdSrv/Tools/Program.cs
+++ IdSrv/Tools/Program.cs
@@ -16,8 +16,24 @@ namespace Tools
     {
         static void Main(string[] args)
         {
-            string password = "Hemmelig!";
-            Console.WriteLine(password.Sha512());
+            string[] tohash = 
+            {
+                "101419822364482428767",
+                "10154276036884453",
+                "870a87c4e5c11bbd",
+                "101431235647359298200",
+                "105064990027889097789",
+                "111915463313837390278",
+                "507c7e6c81f30f81"
+            };
+
+            Console.WriteLine("Raw = Sha512");
+
+            foreach (string raw in tohash)
+            {
+                Console.WriteLine(raw + " = " + raw.Sha512());
+            }
+            
             Console.ReadLine();
         }
     }
