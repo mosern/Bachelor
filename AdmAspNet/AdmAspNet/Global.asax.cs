a9a70a6ab33c6fb3f1a86b0a53aa139a0ff24f7e
mosern <andreas@mosvoll.net>
2016-06-04 23:39:48 +0200

    wasd

--- AdmAspNet/AdmAspNet/Global.asax.cs
+++ AdmAspNet/AdmAspNet/Global.asax.cs
@@ -34,6 +34,16 @@ namespace AdmAspNet
             // Modify current thread's cultures            
             Thread.CurrentThread.CurrentUICulture = Thread.CurrentThread.CurrentCulture;
             Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(culture.Name);
-        } 
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+            /// When using cookie-based session state, ASP.NET does not allocate storage for session data until the Session object is used. 
+            /// As a result, a new session ID is generated for each page request until the session object is accessed. 
+            /// If your application requires a static session ID for the entire session, 
+            /// you can either implement the Session_Start method in the application's Global.asax file and store data in the Session object to fix the session ID, 
+            /// or you can use code in another part of your application to explicitly store data in the Session object.
+            base.Session["init"] = 0;
+        }
     }
 }
